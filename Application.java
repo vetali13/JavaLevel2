b389cfafbc75039fc5d44b630f16c0c19d71c773
vetali13 <66112441+vetali13@users.noreply.github.com>
2020-06-15 13:45:07 +0300

    Add files via upload

--- Application.java
+++ Application.java
@@ -1,64 +1,45 @@
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
+package main;
+
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Arrays;
 
 public class Application {
 
 	public static void main(String[] args) {
-		 ArrayList<Double> temps = new ArrayList<Double>(); 
-		 
-		Double minRange = -25.0d;
-		Double maxRange = 25.0d;
-		Random randomTemperature = new Random();
-		NumberFormat formatter = new DecimalFormat("#0.0");
+		 // CLIENT WISHES
+		ArrayList<String> listOfWishes = new ArrayList<>();
+		listOfWishes.addAll(Arrays.asList(
+				"Spaghetti,2",
+				"Pepper,5",
+				"Sugar,1"
+				));
 		
-		int i = 0;
-		while (i <=31) {
-			Double genTemp = randomTemperature.nextDouble() * (maxRange - minRange) + minRange;
-			temps.add(Double.valueOf(formatter.format(genTemp)));
+		System.out.println(listOfWishes.toString());
+		
+		// STORE OFFERS
+		ArrayList<String> listOfOffers = new ArrayList<>();
+		listOfOffers.addAll(Arrays.asList( 
+			    "Spaghetti,10.00", 
+			    "Sugar,5.00",
+			    "Water,2.50"
+			  )); 
+		 
+		// CLIENT FULFILLED WISHES
+		 ArrayList<String> listOfPurchases = new ArrayList<>();
+		
+		for (int i = 0; i < listOfWishes.size(); i++) {
+			float f1 = 0.0f;
+			for (int j = 0; j < listOfOffers.size(); j++) {
+				
+				if (listOfWishes.get(i).split(",")[0].equals(listOfOffers.get(j).split(",")[0])) {
 			
-			 i++;
-		 }
-
-		String weekDays[] = { "MO", "TU", "WD", "TH", "FR", "SA", "SU" };
-		int counter = 0;
-		for( double t : temps ) {
-			if (counter == 0) {
-				for (String s : weekDays) {
-					System.out.printf("%7s", s);
+					f1 = Float.valueOf(listOfWishes.get(i).split(",")[1]) * 
+							Float.valueOf(listOfOffers.get(j).split(",")[1]);
+					listOfPurchases.add(listOfWishes.get(i) + "=" + f1);
 				}
-				System.out.println();
-				counter++;
 			}
-			System.out.printf("%6.1fC", t);
-			if (counter % 7 == 0) {
-				System.out.println();
-			}
-			counter++;
-		  }
-		  
-		System.out.println("\n");
-		  
-		Double max, min, avg;
-		max = temps.get(0);
-		min = temps.get(0);
-		avg = temps.get(0);
-		Double sum = 0.0d;
-		for( double t: temps ) {
-			  if (t > max) {
-				  max = t;
-			  }
-			  else if (t < min) {
-				  min = t;
-			  }
-			  
-			  sum += t;
-			  avg = sum / temps.size();
 		}
-		  avg = Double.valueOf(formatter.format(avg));
-		  System.out.println("Maximum temperature: " + max + "\n"
-				  + "Minimum temperature: " + min + "\n"
-				  + "Average temperature: " + avg);
-	}	
+		
+		System.out.println(listOfPurchases.toString());
+	}
 }
